ebd83e5875b526b80ac3ee0018daf4cdf8487d6c
ChunFei\Chunfei96 <chunfei96@hotmail.com>
2017-05-05 18:01:10 +0800

    1. Added Texture, Animation, Audio, Prefabs, Scene, Script folders. 2. Created fire scene. 3. Able to place tower on tile.

---
+++ Assets/Script/PlaceMonster.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceMonster : MonoBehaviour {
+
+    public GameObject towerPrefab;
+    private GameObject tower;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+     
+	
+	}
+
+    // Allow only one monster per location
+    private bool canPlacetower()
+    {
+        return tower == null;
+    }
+
+    // Mouse up to place 
+    void OnMouseUp()
+    {
+        if (canPlacetower())
+        {
+            tower = (GameObject)Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+        }     
+    }
+}
